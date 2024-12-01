bc4389ba42dfb06aec1c9a2d555d9a4455833caa
crazyivan1289 <crazyivan1289@yandex.ru>
2024-12-01 11:52:11 +0300

    Fix pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,21 +1,10 @@
-from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 from recipes.constants import DEFAULT_PAGE_SIZE
 
 
-class FoodgramPagination(LimitOffsetPagination):
+class FoodgramPagination(PageNumberPagination):
     """Пагинация для проекта."""
 
-    default_limit = DEFAULT_PAGE_SIZE
     page_size = DEFAULT_PAGE_SIZE
-
-    def get_paginated_response(self, data):
-        return Response(
-            {
-                "count": self.count,
-                "next": self.get_next_link(),
-                "previous": self.get_previous_link(),
-                "results": data,
-            }
-        )
+    page_size_query_param = "limit"
--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -126,6 +126,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.FoodgramPagination',
+    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
